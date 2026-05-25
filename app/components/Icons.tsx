70fb567f132f312449fe6679d878e4fb51ce3c0a
aayush23091 <aaayushyadav0@gmail.com>
2026-05-25 21:31:19 +0545

    Add SVG icon components for forms

---
+++ app/components/Icons.tsx
@@ -0,0 +1,18 @@
+// SVG icon components for use in input fields
+import React from 'react';
+
+export const PersonIcon = () => (
+  <svg width="20" height="20" fill="none" viewBox="0 0 24 24" className="text-gray-400"><path fill="currentColor" d="M12 12c2.7 0 4.5-1.8 4.5-4.5S14.7 3 12 3 7.5 4.8 7.5 7.5 9.3 12 12 12zm0 1.5c-3 0-9 1.5-9 4.5V21h18v-3c0-3-6-4.5-9-4.5z"/></svg>
+);
+
+export const MailIcon = () => (
+  <svg width="20" height="20" fill="none" viewBox="0 0 24 24" className="text-gray-400"><path fill="currentColor" d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 2v.01L12 13 4 6.01V6h16zM4 20V8.99l8 7 8-7V20H4z"/></svg>
+);
+
+export const LockIcon = () => (
+  <svg width="20" height="20" fill="none" viewBox="0 0 24 24" className="text-gray-400"><path fill="currentColor" d="M17 8V7a5 5 0 10-10 0v1a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2v-8a2 2 0 00-2-2zm-8-1a3 3 0 016 0v1H9V7zm8 11H7v-8h10v8zm-5-3a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"/></svg>
+);
+
+export const ArrowForwardIcon = () => (
+  <svg width="20" height="20" fill="none" viewBox="0 0 24 24" className="text-black"><path fill="currentColor" d="M12 4l1.41 1.41L7.83 11H20v2H7.83l5.58 5.59L12 20l-8-8z"/></svg>
+);
